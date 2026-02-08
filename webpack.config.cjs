667007f4625db74afc13855d59bb6d16e804629a
Aleksandr Dzhus <dzhusai25@gmail.com>
2026-02-08 23:37:19 +0300

    Initial setUp

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/Task-07/src/index.js',
+  entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
@@ -11,7 +11,7 @@ module.exports = {
     static: {
       directory: path.join(__dirname, 'public'),
     },
-    port: 9004,
+    port: 9005,
   },
   plugins: [new HtmlWebpackPlugin()],
   module: {
