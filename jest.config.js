79f18e72a2abcd7ebced3cb1292b72202cbed682
Aleksandr Dzhus <dzhusai25@gmail.com>
2026-02-18 15:49:58 +0300

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -3,13 +3,8 @@
  * @type {import('jest').Config}
  */
 const config = {
-  // Среда тестирования (необходима для работы с DOM)
   testEnvironment: 'jsdom',
-
-  // Автоматически очищать моки перед каждым тестом
   clearMocks: true,
-
-  // Покрытие кода
   collectCoverage: false,
   coverageDirectory: 'coverage',
   coverageReporters: ['html', 'text'],
@@ -23,19 +18,16 @@ const config = {
     },
   },
 
-  // Мокирование CSS-файлов (решает ошибку с импортом стилей)
   moduleNameMapper: {
     '\\.css$': '<rootDir>/__mocks__/styleMock.js',
   },
 
-  // Шаблоны для сбора покрытия (включая подпапки)
   collectCoverageFrom: [
     'src/**/*.js',
-    '!src/**/*.test.js', // Исключаем тестовые файлы
-    '!src/**/index.js', // Исключаем индексные файлы, если нужно
+    '!src/**/*.test.js',
+    '!src/**/index.js',
   ],
 
-  // Игнорировать node_modules при трансформации
   transformIgnorePatterns: ['/node_modules/'],
 };
 
